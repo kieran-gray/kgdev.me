f1d3c9e2afd006df4aade59d5be20e60acc79ff4
Kieran Gray <gray.kieran@protonmail.com>
2026-04-28 22:25:04 +0100

    feat: add contact page

--- services/contact-service/test/env.d.ts
+++ services/contact-service/test/env.d.ts
@@ -1,6 +1,6 @@
-import type { D1Migration } from "@cloudflare/vitest-pool-workers/config";
-import type { D1Database } from "@cloudflare/workers-types";
+import type { D1Migration } from '@cloudflare/vitest-pool-workers/config';
+import type { D1Database } from '@cloudflare/workers-types';
 
-declare module "cloudflare:test" {
-  interface ProvidedEnv {}
+declare module 'cloudflare:test' {
+	interface ProvidedEnv {}
 }
